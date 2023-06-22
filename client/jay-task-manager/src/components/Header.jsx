dbc94eb287df0616f5a1086d6c81e6967c47b2ae
jcasare <asarejerry16@gmail.com>
2023-06-22 17:42:53 +0000

    favicon change and header animate commit

--- client/jay-task-manager/src/components/Header.jsx
+++ client/jay-task-manager/src/components/Header.jsx
@@ -1,39 +1,22 @@
-import { useEffect, useRef } from 'react'
-import { useInView } from 'react-intersection-observer'
-
-const AnimatedText = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.5,
-  })
-
-  const paragraphRef = useRef(null) // Create a separate ref for the paragraph element
-
-  useEffect(() => {
-    if (inView) {
-      // Add your animation class or style here
-      paragraphRef.current.classList.add('animate-p')
-    }
-  }, [inView])
-
-  return (
-    <p
-      ref={ref}
-      className="animate-p text-slate-600 font-medium"
-      ref={paragraphRef}
-    >
-      Shake Up Your Task Game with TaskQuake...!!
-    </p>
-  )
-}
-
+import { useEffect } from 'react'
+import '../styles.css'
 const Header = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      const h1Element = document.querySelector('.header-title')
+      if (h1Element) {
+        h1Element.classList.remove('animate-bounce')
+      }
+    }, 2500)
+  }, [])
   return (
     <header>
-      <h1 className="text-4xl font-normal mb-3">
+      <h1 className=" header-title text-4xl font-normal mb-3 animate-bounce">
         <span style={{ color: '#A0E4CB' }}>Task</span>Quake
       </h1>
-      <AnimatedText />
+      <p className="text-slate-600 font-medium">
+        Shake Up Your Task Game With TaskQuake....!!
+      </p>
     </header>
   )
 }
